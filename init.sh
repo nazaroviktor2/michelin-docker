a47b6a410e8150bf7832691555e92cc12dc84bab
nazaroviktor2 <nazarov20012004@gmail.com>
2022-12-20 16:16:37 +0300

    add init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 docker-compose build
+docker-compose run web python3 manage.py collectstatic --noinput
 docker-compose run web python3 manage.py migrate
 docker-compose run web python3 manage.py createsuperuser
